93c556e1f068268588606ed7b4c6609eb780a5df
FARHAN SHAMIM <farzshamim25@gmail.com>
2023-05-19 17:31:46 +0530

    vjhg

---
+++ strings.py
@@ -0,0 +1,6 @@
+story = "hello , how are u doing? its been a long time , lets catch up someday"
+print(story.endswith("someday"))
+print(story.capitalize())
+print(story.count("a"))
+print(type("story"))
+print(story.replace("hello", "hi"))
