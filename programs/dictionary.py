4e1daefcc5805dcaa0d41985949619e8a554d5ec
FARHAN SHAMIM <farzshamim25@gmail.com>
2023-05-19 20:29:52 +0530

    folder added

---
+++ programs/dictionary.py
@@ -0,0 +1,13 @@
+# from typing import KeysView
+
+
+info = {"name" : "Farhanshamim" , "age" : "32" , "qualification" : "M.Tech"}
+# print(info)
+# print(info.keys())
+print(info.items())
+for key,value in info.items():
+    print(f"The value corresponding to the key {key} is {value}")
+    print(" everything is great")
+    print(" everything is great")
+
+hello
